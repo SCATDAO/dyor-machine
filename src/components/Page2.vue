2f9dcbfa7ab8073d7fad9e86c945499d1528a43d
Juan C. Rey <93360398+Kraphene@users.noreply.github.com>
2022-01-13 23:23:13 -0500

    13-01-2022

---
+++ src/components/Page2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="css-dyor-doc-pwo">
+    x
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      d: 0,
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      setTimeout(() => {
+        this.$emit("domRendered");
+      }, 1000);
+    });
+  },
+};
+</script>
+
+<style scoped>
+.css-dyor-doc-pwo{
+  background: #ddf5008f;
+  width: 100%;
+  height: 1099px;
+}
+</style>
