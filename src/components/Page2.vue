19edadd8f6ac6794fc2e11c3c484d92ebdb9aa0b
Juan C. Rey <93360398+Kraphene@users.noreply.github.com>
2022-01-23 14:19:57 -0500

    23-01-2022

--- src/components/Page2.vue
+++ src/components/Page2.vue
@@ -40,6 +40,50 @@
           :series="barData"
         ></apexchart>
       </div>
+      <div class="css-dyor-doc-sdc" style="padding-top: 1rem">
+        <div>
+          <div>
+            <svg
+              width="16"
+              fill="rgb(29, 155, 240)"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+              class="
+                r-1cvl2hr
+                r-4qtqp9
+                r-yyyyoo
+                r-16y2uox
+                r-8kz0gk
+                r-dnmrzs
+                r-bnwqim
+                r-1plcrui
+                r-lrvibr
+                r-lrsllp
+              "
+            >
+              <g>
+                <path
+                  d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"
+                ></path>
+              </g>
+            </svg>
+            <span> - Real followers</span>
+          </div>
+          <span>{{ knowAnswerOption(questionList[16]) }}</span>
+        </div>
+        <div>
+          Reddit ¿Active environment?
+          <span>{{ knowAnswerOption(questionList[18]) }}</span>
+        </div>
+        <div>
+          Telegram ¿Active environment?
+          <span>{{ knowAnswerOption(questionList[20]) }}</span>
+        </div>
+        <div>
+          Discord ¿Active environment?
+          <span>{{ knowAnswerOption(questionList[22]) }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -51,6 +95,7 @@ export default {
     this.evaluatePercentage();
     this.updateDataReport();
     this.updateChartData();
+    this.updateBarData();
   },
   data() {
     return {
@@ -87,7 +132,6 @@ export default {
         ],
         labels: [],
       },
-
       barData: [
         {
           data: [2100, 3200, 532, 1323],
@@ -159,12 +203,7 @@ export default {
           onItemHover: {
             highlightDataSeries: false,
           },
-          customLegendItems: [
-            "1k to 5000k",
-            "1k to 5000k",
-            "1k to 5000k",
-            "1k to 5000k",
-          ],
+          customLegendItems: [],
         },
       },
     };
@@ -190,6 +229,23 @@ export default {
       console.log(labelSorted);
       this.donutData = dataSorted;
       this.donutOption.labels = labelSorted;
+    },
+    updateBarData() {
+      let barData = [
+        this.reportDataDecoded[16].ta,
+        this.reportDataDecoded[18].ta,
+        this.reportDataDecoded[20].ta,
+        this.reportDataDecoded[22].ta,
+      ];
+      this.barData[0].data = barData;
+
+      let barLegend = [];
+
+      for (const x of barData) {
+        barLegend.push(x + " Users");
+      }
+
+      this.barOption.legend.customLegendItems = barLegend;
     },
     updateDataReport() {
       this.reportDataDecoded = this.$store.getters.sendMeReport;
@@ -243,10 +299,15 @@ export default {
   display: flex;
   margin-top: 1rem;
   flex-direction: column;
+  padding-top: 2rem;
 }
 
 .css-dyor-doc-sdc {
   height: 200px;
+}
+
+.css-dyor-doc-ctp {
+  padding-top: 1rem;
 }
 
 .css-dyor-doc-sbc {
