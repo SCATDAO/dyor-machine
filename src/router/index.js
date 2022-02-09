17a2b512bbc020fa9924bb4dfe99c7bf89874874
Juan C. Rey <93360398+Kraphene@users.noreply.github.com>
2022-02-09 04:48:03 -0500

    09-02-2022

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import DyorSummary from "../pages/DyorSummary"
-
+import DyorLanding from "../pages/DyorLanding"
 
 Vue.use(VueRouter)
 
 
 const routes = [
-  { path: '/', component: DyorSummary, props: false },
+  { path: '/', component: DyorLanding, props: false },
   { path: '/report/:id', component: DyorSummary, name: 'report', props: true },
 ]
 
