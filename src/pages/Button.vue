bcf65081f93216a63fefc3db8cffc95589420ea0
Juan C. Rey <93360398+Kraphene@users.noreply.github.com>
2022-02-18 22:37:40 -0500

    18-02-2022

---
+++ src/pages/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <a href="${c}" >Go to report</a>
+</template>
+
+<script>
+export default {
+ data(){
+     return{
+         c: "aaaaaaaa"
+     }
+ }
+}
+</script>
+
+<style>
+a{
+    width: 100px;
+    height: 50px;
+    background: red;
+}
+</style>
